d41159dbd390f99048de2728acd40d1db4887a41
DMArt1984 <dmxart13@gmail.com>
2025-03-12 18:54:46 +0500

    add  Logger

---
+++ DML/Log/LoggerConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DML.Log
+{
+    static public class LoggerConsole
+    {
+        //
+        static public void Log(string message, bool enable = false)
+        {
+            if (enable)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
